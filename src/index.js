994367c3e80bd7870c77e44c0e6a8580f10d55d7
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-05-09 19:36:29 +0300

    commit ready

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log(typeof 'Dog'); // String
+console.log(typeof 20); // Number
+console.log(typeof [1,2]); // object
+console.log(typeof {});// object
+console.log(typeof true);// Boolean
+console.log(typeof undefined); //  undefined
+console.log(typeof null); // null
+console.log(typeof function (){});// function
