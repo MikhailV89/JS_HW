041bd454bdc1fcdd77557a6f616882f13c4fad39
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-05-16 20:28:58 +0300

    commit ready

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-const userNumber = prompt('Enter number');
-const newNumber = userNumber.split('').map(Number);
+const exchangeRate = 26;
 
-let allEqual = newNumber[0] === newNumber[1] && newNumber[1] === newNumber[2];
-let anyEqual = newNumber[0] ===newNumber[1] || newNumber[1] === newNumber[2] || newNumber[0] ===newNumber[2];
-
-console.log("Чи всі цифри однакові:", allEqual ? "Так" : "Ні");
-console.log("Чи є серед цифр цифри однакові:", anyEqual ? "Так" : "Ні");
-
-if (allEqual) {
-    console.log("Всі цифри однакові:", newNumber[0]);
-} else if (anyEqual) {
-    console.log("Серед цифр є однакові:", newNumber[0] === newNumber[1] ? newNumber[0] : (newNumber[0] === newNumber[2] ? newNumber[0] : newNumber[1]));
-} else {
-    console.log("Цифри не повторюються.");
+for (let dollars = 10; dollars <= 100; dollars += 10) {
+    const uaCurrency = dollars * exchangeRate;
+    console.log(`${dollars} dollars = ${uaCurrency} hryvnias`)
 }
