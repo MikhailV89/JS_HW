4c664d3efa67fac2e2c3a029a49985d71b6c9767
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-05-16 19:28:11 +0300

    commit ready

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-const userNumber = prompt('Enter number');
-const newNumber = userNumber.split('').map(Number);
+let result = '';
 
-let allEqual = newNumber[0] === newNumber[1] && newNumber[1] === newNumber[2];
-let anyEqual = newNumber[0] ===newNumber[1] || newNumber[1] === newNumber[2] || newNumber[0] ===newNumber[2];
-
-console.log("Чи всі цифри однакові:", allEqual ? "Так" : "Ні");
-console.log("Чи є серед цифр цифри однакові:", anyEqual ? "Так" : "Ні");
-
-if (allEqual) {
-    console.log("Всі цифри однакові:", newNumber[0]);
-} else if (anyEqual) {
-    console.log("Серед цифр є однакові:", newNumber[0] === newNumber[1] ? newNumber[0] : (newNumber[0] === newNumber[2] ? newNumber[0] : newNumber[1]));
-} else {
-    console.log("Цифри не повторюються.");
+for(let i = 20; i <= 30; i+= 0,5) {
+    result += i + ' ';
 }
+
+console.log(result);
