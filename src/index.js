b4fcd149cf9542b35f5eb47dd196e425774d8799
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-05-17 17:05:49 +0300

    hw ready

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-const userNumber = prompt('Enter number');
-const newNumber = userNumber.split('').map(Number);
+let value = +prompt('Write number from 1 to 100');
+let result = '';
 
-let allEqual = newNumber[0] === newNumber[1] && newNumber[1] === newNumber[2];
-let anyEqual = newNumber[0] ===newNumber[1] || newNumber[1] === newNumber[2] || newNumber[0] ===newNumber[2];
 
-console.log("Чи всі цифри однакові:", allEqual ? "Так" : "Ні");
-console.log("Чи є серед цифр цифри однакові:", anyEqual ? "Так" : "Ні");
-
-if (allEqual) {
-    console.log("Всі цифри однакові:", newNumber[0]);
-} else if (anyEqual) {
-    console.log("Серед цифр є однакові:", newNumber[0] === newNumber[1] ? newNumber[0] : (newNumber[0] === newNumber[2] ? newNumber[0] : newNumber[1]));
-} else {
-    console.log("Цифри не повторюються.");
+for (let i = 1; i <= 100; i++) {
+    if (i * i <= value) {
+        result += i + ', ';
+    }
 }
+
+console.log(result);
+
